338f01891ef4fa3c499bcde68d47fb8025912449
akhil <akhil@geekyants.com>
2026-09-09 12:57:16 +0530

    Added error handler and logger. Moved sensitive information to Environment file. Added tsx to run local development server. updated run script.

--- services/inventory/src/index.ts
+++ services/inventory/src/index.ts
@@ -1,17 +1,23 @@
 import express from "express";
 import { ProductController } from "./productController";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { logger } from "./middleware/logger";
+import { errorHandler } from "./middleware/errorHandler";
+
+dotenv.config();
+
+const mongoUri = process.env.MONGO_URI;
+if (!mongoUri) {
+  throw new Error("Please provide MONGO_URI in the environment variables");
+}
 
 const app = express();
 app.use(express.json());
 
-app.use(() => {
-  console.log("Error handler");
-});
+app.use(errorHandler);
 
-app.use(() => {
-  console.log("Logger");
-});
+app.use(logger);
 
 const productController = new ProductController();
 
@@ -34,10 +40,14 @@ app.delete(
   productController.deleteProduct.bind(productController),
 );
 
-// mongoose
-//   .connect("")
-//   .then(() => {})
-//   .catch(() => {});
+mongoose
+  .connect(mongoUri)
+  .then(() => {
+    console.log("Mongo db connected >>>>");
+  })
+  .catch(() => {
+    console.log("Failed to connect mongo db!");
+  });
 
 app.listen(3003, () => {
   console.log("Product service is running on 3003");
